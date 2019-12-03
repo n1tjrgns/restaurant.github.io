1af958c43afb9be830d4f0d7c2c6e705965f5a56
n1tjrgns <n2tjrgns@gmail.com>
2019-12-03 13:00:45 +0900

    컨트롤러 테스트

---
+++ restaurant-api/src/main/java/com/n1tjrgns/reserve/restaurant/interfaces/RestaurantController.java
@@ -0,0 +1,18 @@
+package com.n1tjrgns.reserve.restaurant.interfaces;
+
+import com.n1tjrgns.reserve.restaurant.domain.Restaurant;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class RestaurantController {
+
+    @GetMapping("/restaurants")
+    public List<Restaurant> list(){
+        List<Restaurant> restaurants = new ArrayList<>();
+        return restaurants;
+    }
+}
---
+++ restaurant-api/src/test/java/com/n1tjrgns/reserve/restaurant/interfaces/RestaurantControllerTest.java
@@ -0,0 +1,26 @@
+package com.n1tjrgns.reserve.restaurant.interfaces;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//컨트롤러 테스트하기
+@RunWith(SpringRunner.class)
+@WebMvcTest(RestaurantControllerTest.class) //테스트 할 클래스 명시
+public class RestaurantControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void list() throws Exception {
+        mvc.perform(get("/restaurants"))
+                .andExpect(status().isOk());
+    }
+}
